ee90cefab5a83c36c36203acbdff24db26bdcc71
Bruno Jimenez <omarjm0910@gmail.com>
2025-11-13 22:38:15 -0600

    mejoras de margenes y paddings

--- app/components/matches/FeaturedMatches.tsx
+++ app/components/matches/FeaturedMatches.tsx
@@ -3,7 +3,7 @@ import FeaturedMatchComponent from "./FeaturedMatchComponent";
 export default function SideMatches() {
   return (
     <>
-      <h2 className="font-headline text-3xl md:text-4xl font-bold leading-tight tracking-tight px-4 pb-4">
+      <h2 className="font-headline text-3xl md:text-4xl font-bold leading-tight tracking-tight px-4 pb-4 mt-2">
         Other Important Matches
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
     <>
       <div className="relative flex h-auto min-h-screen w-full flex-col group/design-root overflow-x-hidden">
         <div className="layout-container flex h-full grow flex-col">
-          <div className="px-4 sm:px-8 md:px-12 lg:px-20 xl:px-40 flex flex-1 justify-center py-5">
+          <div className="px-4 sm:px-8 md:px-8 lg:px-20 xl:px-40 flex flex-1 justify-center">
             <div className="layout-content-container flex flex-col w-full max-w-7xl flex-1">
-              <main className="mt-8 grid grid-cols-1 lg:grid-cols-3 gap-8">
-                <div className="lg:col-span-2 flex flex-col gap-8">
+              <main className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+                <div className="lg:col-span-2 flex flex-col gap-2">
                   <MainMatch />
                   <SideMatches />
                 </div>
